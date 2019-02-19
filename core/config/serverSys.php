9141b872150b11391e5773f01d1af93c2968ba9d
yili <eli.he999@hotmail.com>
2019-02-19 11:04:50 +0800

    <chore>Update some config file
    system infomation

---
+++ core/config/serverSys.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *  local file system
+ */
+
+return array(
+    'HTTP_ENABLE' => true,
+    'HTTPS_ENABLE' => false,
+    'FTP_ENABLE' => false,
+    'TFTP_ENABLE' => false,
+    'FTPS_ENABLE' => false,
+    'TR069' => false,
+    'MQTT' => true,
+
+    /* localtion */
+    'HTTP_PATH' => '',
+    'HTTPS_PATH' => '',
+    'FTPS_PATH' => '',
+    'FTP_PATH' => '',
+    'TFTP_PATH' => '',
+
+    /* REMOTE */
+    'HTTP_REMOTE' => '',
+    'HTTPS_REMOTE' => '',
+    'FTP_REMOTE' => '',
+    'FTPS_REMOTE' => '',
+    'TFTP_REMOTE' => ''
+);
+

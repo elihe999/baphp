3512b85a22812481785c5f16acadbe3b3a1adea4
elihe999 <eli.he999@hotmail.com>
2019-02-22 12:53:54 +0800

    update websocket

--- public/ext/start_websocket.php
+++ public/ext/start_websocket.php
@@ -5,7 +5,7 @@ require_once './../../vendor/workerman/workerman/Autoloader.php';
 // require_once HEART.'/vendor/autoload.php';
 
 
-$ws_worker = new Worker("websocket:/0.0.0.0:2346");
+$ws_worker = new Worker("websocket://192.168.92.88:8000");
 $ws_worker->count = 4;
 
 $ws_worker->onMessage = function($connection, $data)
